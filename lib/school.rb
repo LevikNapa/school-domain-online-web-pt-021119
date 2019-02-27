45b8c353af2a0428b15a26c3f26c5d823b803822
Levik Naparstek <levik010@gmail.com>
2019-02-27 20:42:16 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
 # code here!
+
+class School
+  attr_reader :name, :roster 
+ 
+  
+  def initialize(name)
+  @name = name
+  @roster = {}
+end
+
+def add_student(name, level)
+  roster[level] = []
+  roster[level] << name
+end
+# def grade(level)
+# roster.detect do |y, z|
+#   if y == level
+#     return z
+#   end
+# end
+# end
+def grade(level)
+    roster.detect do |x, y| 
+      if x == level
+        return y 
+      end 
+    end 
+  end 
+  
+  
+  
+  
+end
